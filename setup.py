cf3807a636ebd7761db32d851851675f80a6a652
Jason Casey <j.casey@beamio.net>
2020-06-10 20:39:01 -0400

    initial transfer into new repo

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='TkinterGuiBuilder',
+      version='0.1.0',
+      description='Tools for building GUI interfaces in Tkinter',
+      author='NGA',
+      author_email='j.casey@beamio.net',
+     )
